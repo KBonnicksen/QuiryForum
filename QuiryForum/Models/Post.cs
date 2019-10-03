feae85385a926d775fe603ac70d2816dba34a7b4
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-10-03 12:24:19 -0700

    Established Date and Time posted in Post constructor

--- QuiryForum/Models/Post.cs
+++ QuiryForum/Models/Post.cs
@@ -17,6 +17,7 @@ namespace QuiryForum.Models
         public Post()
         {
             // Set the Account to the account logged in
+            postingDate = DateTime.Now;
         }
 
         /// <summary>
