64eb91a58847ba107be17a0e5398613d4f2d929f
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-10-03 14:01:17 -0700

    Complete follower class

---
+++ QuiryForum/Models/Follower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuiryForum.Models
+{
+    public class Follower
+    {
+        public Follower(int followerID, int followingID)
+        {
+            FollowerID = followerID;
+            FollowingID = followingID;
+        }
+
+        public int FollowerID { get; set; }
+
+        public int FollowingID { get; set; }
+    }
+}
