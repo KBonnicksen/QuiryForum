e49ecd543a0494c64b090f117c8e08cc2eebcbd7
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-10-23 14:42:14 -0700

    Added unit test project

---
+++ QuiryForumTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuiryForum.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuiryForum.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void Index_ReturnsViewResults()
+        {
+            HomeController controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
