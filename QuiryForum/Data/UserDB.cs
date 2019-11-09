45baeb4b8bba6036a67c35087bde266a4253c0aa
KBonnicksen <kelseybon@gmail.com>
2019-11-09 15:21:41 -0800

    changed to Identity user

--- QuiryForum/Data/UserDB.cs
+++ QuiryForum/Data/UserDB.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using QuiryForum.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,17 @@ namespace QuiryForum.Data
 {
     public class UserDB
     {
+        private static readonly UserManager<ApplicationUser> userManager;
+
+        public UserDB(UserManager<ApplicationUser> userManager)
+        {
+            //userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>());
+        }
+        //private UserManager<ApplicationUser> _userManager = new UserManager<ApplicationUser>(IUser)
+        /*public static async Task<ApplicationUser> GetUserByID(string id)
+        {
+            ApplicationUser user = User.
+            return user;
+        }*/
     }
 }
